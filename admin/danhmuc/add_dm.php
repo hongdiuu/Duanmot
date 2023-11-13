01726a51861390c2fb3655868c4f3090e6d59c61
Ha the bao <baotheha@gmail.com>
2023-11-13 17:32:14 +0700

    initial

---
+++ admin/danhmuc/add_dm.php
@@ -0,0 +1,54 @@
+<?php
+include 'boxleft.php';
+?>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Main content -->
+  <section class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <!-- left column -->
+        <div class="col-md-12">
+          <!-- jquery validation -->
+          <div class="card card-primary">
+            <div class="card-header">
+              <h3 class="card-title">Thêm danh mục sản phẩm</h3>
+            </div>
+            <!-- /.card-header -->
+            <!-- form start -->
+            <form id="quickForm">
+              <div class="card-body">
+                <div class="form-group">
+                  <label for="exampleInputEmail1">mã danh mục </label>
+                  <input type="text" name="id_dm" class="form-control" id="exampleInputEmail1" placeholder="id danh mục">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Tên danh mục</label>
+                  <input type="text" name="ten_dm" class="form-control" id="exampleInputPassword1" placeholder="Tên danh mục sản phẩm">
+                </div>
+               
+              </div>
+              <!-- /.card-body -->
+              <div class="card-footer">
+                <input type="button" class="btn btn-primary" value="Thêm Mới">
+               
+                <button type="reset" class="btn btn-primary">Nhập lại</button>
+               <a href="index.php?act=list_dm">   <input type="button" class="btn btn-primary" value="Danh sách"></a>
+              </div>
+            </form>
+          </div>
+          <!-- /.card -->
+        </div>
+        <!--/.col (left) -->
+        <!-- right column -->
+        <div class="col-md-6">
+
+        </div>
+        <!--/.col (right) -->
+      </div>
+      <!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </section>
+  <!-- /.content -->
+
+</div>
---
+++ admin/home.php
@@ -0,0 +1,7 @@
+<?php
+include 'boxleft.php';
+?>
+  <!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <h1>conpatrol</h1>
+</div>
